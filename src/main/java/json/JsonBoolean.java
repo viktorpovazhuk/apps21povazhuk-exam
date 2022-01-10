12ab0cad46d16a74da3c2282d6eac210bf5c7292
vityha <viktor.povazhuk@ucu.edu.ua>
2022-01-10 11:11:06 +0200

    Implemented 1 part

--- src/main/java/json/JsonBoolean.java
+++ src/main/java/json/JsonBoolean.java
@@ -4,14 +4,14 @@ package json;
  * Created by Andrii_Rodionov on 1/4/2017.
  */
 public class JsonBoolean extends Json {
+    private final Boolean bool;
 
     public JsonBoolean(Boolean bool) {
-        // ToDo
+        this.bool = bool;
     }
 
     @Override
     public String toJson() {
-        // ToDo
-        return null;
+        return bool.toString();
     }
 }
--- src/main/java/json/JsonObject.java
+++ src/main/java/json/JsonObject.java
@@ -1,18 +1,50 @@
 package json;
 
+import java.util.*;
+
 /**
  * Created by Andrii_Rodionov on 1/3/2017.
  */
 public class JsonObject extends Json {
+    private final Map<String, Json> jsonFields;
 
     public JsonObject(JsonPair... jsonPairs) {
-        // ToDo
+        List<JsonPair> pairs = Arrays.asList(jsonPairs);
+        jsonFields = new HashMap<>();
+        for (JsonPair jsonPair: pairs) {
+            jsonFields.put(jsonPair.key, jsonPair.value);
+        }
     }
 
     @Override
     public String toJson() {
-        // ToDo
-        return null;
+        return "{" + getJsonObjectBody() + "}";
+    }
+
+    private String getJsonObjectBody() {
+        StringBuilder jsonStr = new StringBuilder();
+
+        Iterator<Map.Entry<String, Json>> iterator = jsonFields.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Json> jsonField = iterator.next();
+            jsonStr.append("'").append(jsonField.getKey()).append("': ");
+            jsonStr.append(jsonField.getValue().toJson());
+            if (iterator.hasNext()) {
+                jsonStr.append(", ");
+            }
+        }
+
+//        Iterator<JsonPair> jsonIterator = jsonPairs.iterator();
+//        while (jsonIterator.hasNext()) {
+//            JsonPair jsonPair = jsonIterator.next();
+//            jsonStr.append("'").append(jsonPair.key).append("': ");
+//            jsonStr.append(jsonPair.value.toJson());
+//            if (jsonIterator.hasNext()) {
+//                jsonStr.append(", ");
+//            }
+//        }
+
+        return jsonStr.toString();
     }
 
     public void add(JsonPair jsonPair) {
